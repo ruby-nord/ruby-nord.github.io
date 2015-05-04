b9ecb0addf149afa78e50377ae601e460e154e8e
Cecile Veneziani <zora@patapouf.org>
2015-05-04 09:36:43 +0200

    Change deploy strategy
    - Add middleman-deploy
    - Configure middleman deploy to push on master branch
    - Remove middleman-gh-pages

--- config.rb
+++ config.rb
@@ -1,10 +1,13 @@
 require 'slim'
 
-activate :autoprefixer,
-  browsers: ['last 2 versions', 'ie 8', 'ie 9']
-
-activate :livereload
+activate :autoprefixer, browsers: ['last 2 versions', 'ie 8', 'ie 9']
 activate :directory_indexes
+activate :livereload
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = :master
+end
 
 set :js_dir, 'assets/javascripts'
 set :css_dir, 'assets/stylesheets'
@@ -12,15 +15,12 @@ set :images_dir, 'assets/images'
 
 # Add bower's directory to sprockets asset path
 after_configuration do
-
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
   sprockets.append_path File.join "#{root}", @bower_config["directory"]
-
 end
 
 # Build-specific configuration
 configure :build do
-
   activate :favicon_maker do |f|
     f.template_dir  = File.join(root, 'source')
     f.output_dir    = File.join(root, 'build')
@@ -57,5 +57,4 @@ configure :build do
   activate :asset_hash
   # activate :relative_assets
   # set :relative_links, true
-
 end
